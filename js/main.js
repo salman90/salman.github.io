492ebb726773785388ce304955984d0899d06c53
salman90 <salmansalem9022@gmail.com>
2017-02-10 17:01:42 -0800

    profile

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var scroll_start = 0;
+  var startchange = $('#section1');
+  var offset = startchange.offset();
+  if (startchange.length){
+    $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if (scroll_start > offset.top) {
+        $('nav.navbar-default').css('background-color', '#FFFFFF');
+        $('nav.navbar-default .navbar-nav>li>a, nav.navbar-default .navbar-brand').css('color', 'black');
+      } else {
+        $('nav.navbar-default').css('background-color', 'transparent');
+        $('nav.navbar-default .navbar-nav>li>a, nav.navbar-default .navbar-brand').css('color', 'white');
+      }
+    });
+  }
+});
