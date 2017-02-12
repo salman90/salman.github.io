3f1235f680a0b1c54c38aaac59e9f6b21f352dc4
salman90 <salmansalem9022@gmail.com>
2017-02-11 16:53:30 -0800

    styling

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
     $(document).scroll(function() {
       scroll_start = $(this).scrollTop();
       if (scroll_start > offset.top) {
-        $('nav.navbar-default').css('background-color', '#FFFFFF');
-        $('nav.navbar-default .navbar-nav>li>a, nav.navbar-default .navbar-brand').css('color', 'black');
+        $('nav.navbar-default').css('background-color', '#ffffff');
+        $('nav.navbar-default .navbar-nav>li>a, nav.navbar-default .navbar-brand').css('color', '#000');
       } else {
         $('nav.navbar-default').css('background-color', 'transparent');
-        $('nav.navbar-default .navbar-nav>li>a, nav.navbar-default .navbar-brand').css('color', 'white');
+        $('nav.navbar-default .navbar-nav>li>a, nav.navbar-default .navbar-brand').css('color', '#000');
       }
     });
   }
