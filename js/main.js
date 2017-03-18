f4fa0cdab485a8908e6b979a7175e121e9643b4c
salman90 <salmansalem9022@gmail.com>
2017-03-18 15:18:41 -0700

    nav

--- js/main.js
+++ js/main.js
@@ -16,15 +16,10 @@ $(document).ready(function(){
   }
 });
 
-function collapseNavOnClick() {
-  $(".navbar-nav li a").click(function(event) {
-    $(".navbar-collapse").collapse('hide');
-  });
-}
+
 
 
 function respond() {
-  collapseNavOnClick();
 }
 
 $(document).ready(function() {
